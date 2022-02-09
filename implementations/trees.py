bcef1719ba751635fe3f8c29bd97cea0f84a1647
roycehoe <royceinbox@hotmail.com>
2022-02-09 21:21:12 +0800

    Give up hidingplaces challenge for now

---
+++ implementations/trees.py
@@ -0,0 +1,6 @@
+"""
+https://www.youtube.com/watch?v=fAAZixBzIAI
+
+Needs drawing. Refer to my notion page. Will make public when I feel like it, maybe never.
+https://www.notion.so/Trees-82430e06a5a74005a807b8e76b4862f7
+"""
