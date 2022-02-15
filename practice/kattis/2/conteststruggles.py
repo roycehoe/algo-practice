1fd5a4d1a610f982ea2a807284a0dfd99fa6c9ad
roycehoe <royceinbox@hotmail.com>
2022-02-16 01:32:45 +0800

    give up conteststruggles

---
+++ practice/kattis/2/conteststruggles.py
@@ -0,0 +1,20 @@
+"""https://open.kattis.com/problems/conteststruggles"""
+
+n, k = input("").split()
+d, s = input("").split()
+
+n = int(n)
+k = int(k)
+d = int(d)
+s = int(s)
+
+if d == 0:
+    print(0)
+else:
+    dif_sum = n * d
+    solved_sum = s * k
+    remaining_qns = n - k
+
+    ans = (dif_sum - solved_sum)/remaining_qns
+
+    print(ans if ans <= 100 else "impossible")
