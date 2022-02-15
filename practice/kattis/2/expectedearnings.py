aa45ba95d12876a26eb596d8b40ea92df32c2583
roycehoe <royceinbox@hotmail.com>
2022-02-15 16:41:35 +0800

    Finish expectedearnings; found careless mistake; forgot to account for cost to play

--- practice/kattis/2/expectedearnings.py
+++ practice/kattis/2/expectedearnings.py
@@ -7,12 +7,17 @@ n = int(n)
 k = int(k)
 p = float(p)
 
-# if (p*n*k) < 0:
-#     print("spela")
-# else:
-#     print("spela inte!")
 
-if p < float(0.500000000):
+def is_negative_e(n, k, p):
+    win = n - k
+    lose = k
+    e = (win * p) - (lose * (1-p))
+    if e < 0:
+        return True
+    return False
+
+
+if is_negative_e(n, k, p):
     print("spela")
 else:
     print("spela inte!")
