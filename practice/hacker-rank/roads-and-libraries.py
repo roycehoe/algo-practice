521742b1d5ea3769409af84c7a372212b719da1a
roycehoe <royceinbox@hotmail.com>
2022-02-17 21:27:05 +0800

    Partial solve for roads and library; fitting user input

--- practice/hacker-rank/roads-and-libraries.py
+++ practice/hacker-rank/roads-and-libraries.py
@@ -1,84 +1,83 @@
 """https://www.hackerrank.com/challenges/torque-and-development/problem?isFullScreen=true"""
 
-"""
-Steps
- - Create adj list
- - Get all connected components
- - For each connected component:
-  - Get cost of building 1 library + minimum spanning treee
-  - Get cost of building library in all cities
-  - Return the minimum for this
-  - Sum up minimum for all connected components
-"""
+
+def is_existing_node(dict, parent):
+    return False if dict.get(parent) is None else True
 
 
-c_road = 2  # cost of building road
-c_lib = 3  # cost of building library
-cities = [[1, 2], [3, 1], [2, 3]]
+def create_node(dict, parent, child):
+    dict[parent] = [child]
+    return dict
 
 
-def get_build_library_cost(adj_list, c_lib):
-    nodes = len(adj_list)
-    MST_paths = nodes - 1
-    return c_lib * MST_paths
+def extend_node(dict, parent, child):
+    temp = dict.get(parent)
+    temp.append(child)
+    dict[parent] = temp
+    return dict
 
 
-def get_build_roads_cost(adj_list, c_road, c_lib):
-    nodes = len(adj_list)
-    MST_paths = nodes - 1
-    return (c_lib + c_road * MST_paths)
+def add_node(dict, parent, child):
+    if is_existing_node(dict, parent):
+        return extend_node(dict, parent, child)
+    return create_node(dict, parent, child)
 
 
-def get_cheapest_build(adj_list, c_road, c_lib):
-    build_roads_cost = get_build_roads_cost(adj_list, c_road, c_lib)
-    build_library_cost = get_build_library_cost(adj_list, c_lib)
-    return build_roads_cost if build_roads_cost < build_library_cost else build_library_cost
-
-
-def create_adj_list(cities):
-    adj_list = {}
-    for i in cities:
-        adj_list[i[0]] = [i[1]]
-    return adj_list
-
-
-def create_component_list(adj_list):
-    component_list = []
+def get_connected_nodes(nodes, start):
     visited = set()
 
-    for current_node in adj_list:
-        if current_node not in visited:
-            visited.add(current_node)
+    stack = [start]
 
-        neighbour_nodes = adj_list.get(current_node)
-        for neighbour_node in neighbour_nodes:
-            if neighbour_node not in visited:
-                visited.add(neighbour_node)
+    while len(stack) > 0:
+        parent = stack.pop()
+        if parent not in visited:
+            visited.add(parent)
 
+            children = nodes.get(parent)
+            for child in children:
+                stack.append(child)
 
-test = create_adj_list(cities)
-print(test)
-
-
-def main(number_of_cities, c_lib, c_road, city_array):
-    pass
+    return visited
 
 
-"""
+def get_cheapest_option(path, c_road, c_lib):
+    tc_road = (len(path) - 1) * c_road
+    tc_lib = len(path) * c_lib
+    return tc_lib if tc_lib < tc_road else tc_road
 
-n, m, c_lib, c_road = input("").split()
-n = int(n)
-m = int(m)
-c_lib = int(c_lib)
-c_road = int(c_road)
-cities = []
+
+#########
+
+
+n = 6
+
+c_lib = 2
+c_road = 5
+ans = 0
+
+nodes = {}
+
+paths = []
+visited = set()
+
 
 for i in range(0, n):
-    first_node, second_node = input("").split()
-    first_node = int(first_node)
-    second_node = int(second_node)
-    cities.append([first_node, second_node])
+    parent, child = input().split()
+    parent = int(parent)
+    child = int(child)
+    nodes = add_node(nodes, parent, child)
+    nodes = add_node(nodes, child, parent)
 
-print(cities)
 
-"""
+for node in nodes:
+    if node not in visited:
+        connected_nodes = get_connected_nodes(nodes, node)
+        for connected_node in connected_nodes:
+            visited.add(connected_node)
+        paths.append(connected_nodes)
+
+
+for path in paths:
+    ans += get_cheapest_option(path, c_road, c_lib)
+
+print(ans)
