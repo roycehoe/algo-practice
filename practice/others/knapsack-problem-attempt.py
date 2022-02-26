cf2f64ed3bb1f87415fdd149217fecd5dbd2e57a
roycehoe <royceinbox@hotmail.com>
2022-02-26 14:37:28 +0800

    Found coin change solution online; to understand, reverse engineer, and solve without referring to solution

--- practice/others/knapsack-problem-attempt.py
+++ practice/others/knapsack-problem-attempt.py
@@ -1,41 +1,36 @@
 """
-https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/
-https://www.youtube.com/watch?v=nLmhmB6NzcM&t=706s
 """
-
-
-def get_max_profit(table, weight, item_index, weight_index):
-    profit_without_item = table[item_index-1][weight_index]
-
-    item_profit = profits[item_index-1]
-    profit_with_item = item_profit + \
-        table[item_index-1][weight_index - weight[item_index-1]]
-
-    return max(profit_with_item, profit_without_item)
-
-
-def get_max_knapsack_profit(capacity, weight, profits):
-    item_count = len(profits)
-    table = [[0 for i in range(capacity+1)] for i in range(item_count+1)]
-
-    for item_index in range(item_count+1):
-        for weight_index in range(capacity + 1):
-            if item_index == 0 or weight_index == 0:
-                table[item_index][weight_index] = 0
-
-            elif weight[item_index-1] <= weight_index:
-                table[item_index][weight_index] = get_max_profit(
-                    table, weight, item_index, weight_index)
-            else:
-                table[item_index][weight_index] = table[item_index-1][weight_index]
-
-    return table[item_count][capacity]
-
-####################################################
-
 
 profits = [60, 100, 120]
 weights = [10, 20, 30]
 capacity = 50
-print(get_max_knapsack_profit(capacity, weights, profits))
-# print(create_dp_table(knapsack_capacity, len(items)))
+
+
+"""
+table[capacity_index][item_index]
+"""
+
+item_count = len(profits)
+table = [[0 for i in range(capacity + 1)] for i in range(item_count+1)]
+
+for item_index in range(item_count+1):
+    for capacity_index in range(capacity+1):
+
+        if capacity_index == 0 or item_index == 0:
+            table[item_index][capacity_index] = 0
+
+        elif capacity_index > weights[item_index-1]:
+            item_profit = profits[item_index-1]
+            item_weight = weights[item_index - 1]
+
+            profit_with_item = item_profit + \
+                table[item_index][capacity_index-item_weight]
+
+            profit_without_item = table[item_index-1][capacity_index]
+
+            table[capacity_index][item_index] = max(
+                profit_with_item, profit_without_item)
+        else:
+            table[capacity_index][item_index] = table[capacity_index][item_index-1]
+
+print(table)
