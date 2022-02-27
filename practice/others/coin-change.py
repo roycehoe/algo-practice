efafcac3dc9c9b32d424abd0eb020cdfbd0047c7
roycehoe <royceinbox@hotmail.com>
2022-02-27 17:15:01 +0800

    Solved coin challenge on my own; to learn, reverse engineer and test more efficient solution

--- practice/others/coin-change.py
+++ practice/others/coin-change.py
@@ -4,10 +4,11 @@ coin_values = [2, 5, 3, 6]
 coin_count = len(coin_values)
 available_change = 10
 
-table = [[0 for y in range(available_change+1)] for x in range(coin_count + 1)]
+table = [[0 for current_change in range(
+    available_change+1)] for coin_index in range(coin_count+1)]
 
-for current_change in range(available_change + 1):
-    for coin_index in range(coin_count + 1):
+for coin_index in range(coin_count+1):
+    for current_change in range(available_change+1):
 
         if current_change == 0:
             table[coin_index][current_change] = 1
@@ -15,15 +16,13 @@ for current_change in range(available_change + 1):
             table[coin_index][current_change] = 0
         else:
             ans = 0
-            current_coin_value = coin_values[coin_index - 1]
-
-            if current_change >= current_coin_value:
-                previous_change_index = current_change - current_coin_value
-                ans += table[coin_index][previous_change_index]
+            current_coin_value = coin_values[coin_index-1]
 
             if coin_index >= 1:
-                previous_coin_index = coin_index - 1
-                ans += table[previous_coin_index][current_change]
+                ans += table[coin_index - 1][current_change]
+
+            if current_change >= current_coin_value:
+                ans += table[coin_index][current_change - current_coin_value]
 
             table[coin_index][current_change] = ans
 
