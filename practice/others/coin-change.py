bd9b12946b509bf70cd4dae397bed1576a6fe128
roycehoe <royceinbox@hotmail.com>
2022-02-27 17:05:27 +0800

    Solve coin challenge; to redo to test understanding

--- practice/others/coin-change.py
+++ practice/others/coin-change.py
@@ -1,38 +1,31 @@
 """https://www.geeksforgeeks.org/coin-change-dp-7/"""
 
-
-def get_coin_change_count(coin_values, coin_count, available_change):
-    table = [[0 for coin_index in range(coin_count)]
-             for change_index in range(available_change+1)]
-
-    # solution is 1 when change_index is zero, regardless of coin index
-    for coin_index in range(coin_count):
-        table[0][coin_index] = 1
-
-    for current_change in range(1, available_change+1):
-        for coin_index in range(coin_count):
-            ans = 0
-            current_coin_value = coin_values[coin_index]
-
-            # Take the answer from the row above, same column [ie. takes previous ans]
-            if current_change >= current_coin_value:
-                ans += table[current_change - current_coin_value][coin_index]
-
-            # Take the answer from the row above, same column [ie. takes previous ans]
-            if coin_index >= 1:
-                ans += table[current_change][coin_index-1]
-
-            table[current_change][coin_index] = ans
-
-    return table
-    # return table[available_change][coin_count-1]
-
-
-# Driver program to test above function
 coin_values = [2, 5, 3, 6]
 coin_count = len(coin_values)
 available_change = 10
 
+table = [[0 for y in range(available_change+1)] for x in range(coin_count + 1)]
 
-ans = get_coin_change_count(coin_values, coin_count, available_change)
-print(ans)
+for current_change in range(available_change + 1):
+    for coin_index in range(coin_count + 1):
+
+        if current_change == 0:
+            table[coin_index][current_change] = 1
+        elif coin_index == 0:
+            table[coin_index][current_change] = 0
+        else:
+            ans = 0
+            current_coin_value = coin_values[coin_index - 1]
+
+            if current_change >= current_coin_value:
+                previous_change_index = current_change - current_coin_value
+                ans += table[coin_index][previous_change_index]
+
+            if coin_index >= 1:
+                previous_coin_index = coin_index - 1
+                ans += table[previous_coin_index][current_change]
+
+            table[coin_index][current_change] = ans
+
+
+print(table)
