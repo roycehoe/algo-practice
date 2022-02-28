1621d9c8f079ffcb54cb35e77645c8c7e6765f58
roycehoe <royceinbox@hotmail.com>
2022-02-28 09:49:31 +0800

    Understand coin change efficient solution via printing variables; to deconstruct solution further to deepen understanding

--- practice/others/coin-change.py
+++ practice/others/coin-change.py
@@ -1,30 +1,22 @@
 """https://www.geeksforgeeks.org/coin-change-dp-7/"""
 
-coin_values = [2, 5, 3, 6]
+coin_values = [99, 2, 5, 3, 6]
 coin_count = len(coin_values)
 available_change = 10
 
-table = [[0 for current_change in range(
-    available_change+1)] for coin_index in range(coin_count+1)]
+######Efficient solution###########
 
-for coin_index in range(coin_count+1):
-    for current_change in range(available_change+1):
+table = [0 for change_index in range(available_change+1)]
+table[0] = 1
 
-        if current_change == 0:
-            table[coin_index][current_change] = 1
-        elif coin_index == 0:
-            table[coin_index][current_change] = 0
-        else:
-            ans = 0
-            current_coin_value = coin_values[coin_index-1]
+for coin_index in range(0, coin_count):
+    current_coin_value = coin_values[coin_index]
 
-            if coin_index >= 1:
-                ans += table[coin_index - 1][current_change]
-
-            if current_change >= current_coin_value:
-                ans += table[coin_index][current_change - current_coin_value]
-
-            table[coin_index][current_change] = ans
-
-
-print(table)
+    for change_index in range(current_coin_value, available_change+1):
+        table[change_index] += table[change_index - current_coin_value]
+    print(f'''
+Current coin value: {current_coin_value}
+Start index: {current_coin_value}
+End index: {available_change+1}
+Table: {table}
+    ''')
