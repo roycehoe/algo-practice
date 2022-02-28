367af9329dac33365c26520ae2386daf99b79dce
roycehoe <royceinbox@hotmail.com>
2022-02-28 10:19:42 +0800

    Solve coin-change on hackerrank; to learn different sorting algos

--- practice/hacker-rank/coin-challenge.py
+++ practice/hacker-rank/coin-challenge.py
@@ -1,47 +1,17 @@
 """https://www.hackerrank.com/challenges/coin-change/problem?isFullScreen=true"""
 
-"""https://www.ideserve.co.in/learn/coin-change-problem-number-of-ways-to-make-change"""
-
-"""
-n = 50
-c = {1,5,10,20}
-
-ans = 
-get ways to make change of 50 using 0 coins of 20
-+ get ways to make change of 50 using 1 coin of 20 with {1,5,10}
-+ get ways to make change of 50 using 2 coins of 20 with {1,5,10}
-
-ans = 
-+ get ways to make change of 50 using 0 coins of 20 with {1,5,10}
-+ get ways to make change of 30 using 0 coins of 20 with {1,5,10}
-+ get ways to make change of 10 using 0 coins of 20 with {1,5,10}
-
-"""
-
-
-from re import L
 n = 3
 c = [8, 3, 1, 2]
 
-n = 4
-c = [1, 2, 3]
+c_count = len(c)
+ans_table = [0 for i in range(n+1)]
+ans_table[0] = 1
+print(ans_table)
 
-# n = 50
-# c = [20, 10, 5, 1]
+for coin_index in range(c_count):
+    current_coin_value = c[coin_index]
 
+    for ans_index in range(current_coin_value, n+1):
+        ans_table[ans_index] += ans_table[ans_index - current_coin_value]
 
-# first iteration
-
-
-def get_change(n, c):
-    first_num = c[0]
-    remainder = n
-    while remainder >= 0:
-        print(remainder, "call recursive fn")
-        remainder = remainder - first_num
-        # do something with the remainder
-
-# first iteration
-
-
-print(get_change(n, c))
+print(ans_table[n])
